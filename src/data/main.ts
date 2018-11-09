0ed93b7a734cf918bbd396919b0c35e14b9c3c46
Ivar Nesje <ivarne@gmail.com>
2018-11-09 07:43:05 +0100

    Added simple cors hack with a new azure functions site

--- src/data/main.ts
+++ src/data/main.ts
@@ -8,7 +8,7 @@ function getMain(result: (data: Main) => void) {
   if (mainCache) return result(mainCache);
   if (mainFetchStarted) return;
   mainFetchStarted = true;
-  fetch("https://sammenlign.utdanning.no/rest/main")
+  fetch("https://cors-hack-sammenligning.azurewebsites.net/rest/main")
     .then(response => response.json())
     .then(data => {
       mainCache = data;
@@ -26,9 +26,8 @@ function getGeneric(result: (data: DataList) => void, prefix: "u" | "y" | "s") {
     const list = Object.keys(main)
       .filter(key => key[0] === prefix)
       .map(key => main[key])
-      .sort(
-        (a: MainElement, b: MainElement) =>
-          a.tittel < b.tittel ? -1 : a.tittel > b.tittel ? 1 : 0
+      .sort((a: MainElement, b: MainElement) =>
+        a.tittel < b.tittel ? -1 : a.tittel > b.tittel ? 1 : 0
       );
     const interesser: { [key: string]: boolean } = {};
     list.forEach((el: MainElement) => {
