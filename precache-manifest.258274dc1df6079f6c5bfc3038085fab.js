3f94c38a3ab9a1f92cba0fee4eb59e22574a3830
Ivar Nesje <ivarne@gmail.com>
2018-11-15 12:22:20 +0100

    Updates

---
+++ precache-manifest.258274dc1df6079f6c5bfc3038085fab.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "9b08535ca26357771c42",
+    "url": "/sammenlignfrontend/static/css/main.c8059609.chunk.css"
+  },
+  {
+    "revision": "9b08535ca26357771c42",
+    "url": "/sammenlignfrontend/static/js/main.9b08535c.chunk.js"
+  },
+  {
+    "revision": "01a1ba37c20b4d6a4274",
+    "url": "/sammenlignfrontend/static/js/1.01a1ba37.chunk.js"
+  },
+  {
+    "revision": "e8bb3c6c13f4faa646cd",
+    "url": "/sammenlignfrontend/static/js/runtime~main.e8bb3c6c.js"
+  },
+  {
+    "revision": "b273434b499c8ddcfaed7995c3640fb9",
+    "url": "/sammenlignfrontend/static/media/NoData.b273434b.svg"
+  },
+  {
+    "revision": "1550a2a3b5c0646e8dc386a30787f683",
+    "url": "/sammenlignfrontend/static/media/Smiley.1550a2a3.svg"
+  },
+  {
+    "revision": "5bfeee98132c6c8404e354498e754d82",
+    "url": "/sammenlignfrontend/static/media/Mustache.5bfeee98.svg"
+  },
+  {
+    "revision": "5d2305726dafd43fdc954f1a7e3f56ba",
+    "url": "/sammenlignfrontend/index.html"
+  }
+];
