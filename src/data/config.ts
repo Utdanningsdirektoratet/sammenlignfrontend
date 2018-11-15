12cca9409af11e57b42c849241e303c22d91ff92
Ivar Nesje <ivar.nesje@bouvet.no>
2018-11-15 11:59:58 +0100

    Remove cors-hack

--- src/data/config.ts
+++ src/data/config.ts
@@ -1,1 +1,2 @@
-export const API_DOMAIN = "https://cors-hack-sammenligning.azurewebsites.net";
+// export const API_DOMAIN = "https://cors-hack-sammenligning.azurewebsites.net";
+export const API_DOMAIN = "https://sammenlign.utdanning.no";
