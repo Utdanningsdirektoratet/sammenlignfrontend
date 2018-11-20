1278c1b289ba8011d1112010266642f6c3176e76
Kristin Annabel Folland <kristinannabel.folland@gmail.com>
2018-11-20 14:31:27 +0100

    Responsive design for Selected uno id boxes for all screen sizes.

--- src/components/pages/Shared/SelectedCompares.tsx
+++ src/components/pages/Shared/SelectedCompares.tsx
@@ -6,9 +6,21 @@ import { Innholdstype } from "../../../data/ApiTypes";
 import { with_app_state, AppStateProps } from "../../app/AppContext";
 import CloseIcon from "../../visualizations/Generic/CloseIcon";
 import UnoId from "../../app/UnoId";
+import { Grid, Row, Col } from "react-bootstrap";
 
 type Props = {
   innholdstype: Innholdstype;
+};
+
+type CellCalcs = {
+  xs: number;
+  xsOffset: number;
+  sm: number;
+  smOffset: number;
+  md: number;
+  mdOffset: number;
+  lg: number;
+  lgOffset: number;
 };
 
 class SelectedCompares extends Component<AppStateProps & Props> {
@@ -32,19 +44,66 @@ class SelectedCompares extends Component<AppStateProps & Props> {
     if (filtered_uno_id.length === 0) {
       return null;
     }
-
     return (
       <>
         <div className={`${styles.selection}`}>
           <ul className={`${styles.selection_row}`}>
-            {filtered_uno_id.map(uno_id => (
-              <li key={uno_id} className={`${styles.selection_row_item}`}>
-                <div className={`${styles.selection_row_item_text}`}>
-                  <UnoId uno_id={uno_id} />
-                </div>
-                <CloseIcon unoId={uno_id} onClick={this.handleRemoveClick} />
-              </li>
-            ))}
+            <Row>
+              {filtered_uno_id.map((uno_id: string, i: number) => (
+                <Col
+                  xs={filtered_uno_id.length === 3 ? 4 : 6}
+                  xsOffset={
+                    i === 0 ? (filtered_uno_id.length === 1 ? 3 : 0) : 0
+                  }
+                  sm={filtered_uno_id.length < 4 ? 4 : 3}
+                  smOffset={
+                    i === 0
+                      ? filtered_uno_id.length === 1
+                        ? 4
+                        : filtered_uno_id.length === 2
+                        ? 2
+                        : 0
+                      : 0
+                  }
+                  md={filtered_uno_id.length < 4 ? 4 : 3}
+                  mdOffset={
+                    i === 0
+                      ? filtered_uno_id.length === 1
+                        ? 4
+                        : filtered_uno_id.length === 2
+                        ? 2
+                        : 0
+                      : 0
+                  }
+                  lg={2}
+                  lgOffset={
+                    i === 0
+                      ? filtered_uno_id.length === 1
+                        ? 5
+                        : filtered_uno_id.length === 2
+                        ? 4
+                        : filtered_uno_id.length === 3
+                        ? 3
+                        : filtered_uno_id.length === 4
+                        ? 2
+                        : filtered_uno_id.length === 5
+                        ? 1
+                        : 0
+                      : 0
+                  }
+                >
+                  <li key={uno_id} className={`${styles.selection_row_item}`}>
+                    <div className={`${styles.selection_row_item_text}`}>
+                      <UnoId uno_id={uno_id} />
+                    </div>
+                    <CloseIcon
+                      unoId={uno_id}
+                      onClick={this.handleRemoveClick}
+                    />
+                  </li>
+                </Col>
+              ))}
+            </Row>
           </ul>
         </div>
       </>
