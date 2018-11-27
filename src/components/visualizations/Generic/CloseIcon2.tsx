6593b58d2f4adbc460865e3967fc9865d216ff7e
Ivar Nesje <ivar.nesje@bouvet.no>
2018-11-27 14:34:31 +0100

    Removed onClick handler for CloseIcon

--- src/components/visualizations/Generic/CloseIcon2.tsx
+++ src/components/visualizations/Generic/CloseIcon2.tsx
@@ -1,20 +1,11 @@
 import React, { Component } from "react";
 import styles from "./CloseIcon.module.scss";
 
-type Props = {
-  onClick: (event: any) => void;
-  unoId: string;
-};
+type Props = {};
 
 class CloseIcon2 extends Component<Props> {
   render() {
-    return (
-      <span
-        className={`${styles.icon_close2}`}
-        data-uno_id={this.props.unoId}
-        onClick={this.props.onClick}
-      />
-    );
+    return <span className={`${styles.icon_close2}`} />;
   }
 }
 
