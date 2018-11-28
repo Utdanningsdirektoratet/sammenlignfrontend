b70f341f254252a230eaafdf950132af4219d517
Ivar Nesje <ivarne@gmail.com>
2018-11-28 09:05:39 +0100

    Updates

---
+++ precache-manifest.3bdcf5e68713b82c99c1f37a23b58dab.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee644238e0039fa820c",
+    "url": "/static/css/main.32f665c3.chunk.css"
+  },
+  {
+    "revision": "fee644238e0039fa820c",
+    "url": "/static/js/main.fee64423.chunk.js"
+  },
+  {
+    "revision": "9a4e55a4325f7ac24c91",
+    "url": "/static/js/1.9a4e55a4.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "d55b5667b28e5f9c083713f20af7543a",
+    "url": "/static/media/exclamation-circle.d55b5667.svg"
+  },
+  {
+    "revision": "e49dedbf13f7da9577aa127b06577e68",
+    "url": "/static/media/search.e49dedbf.svg"
+  },
+  {
+    "revision": "9aa10d0507505460ad3fef9ce273b395",
+    "url": "/static/media/times.9aa10d05.svg"
+  },
+  {
+    "revision": "b0ca7d8035e50297ae530cab9f85b139",
+    "url": "/static/media/chevron-down.b0ca7d80.svg"
+  },
+  {
+    "revision": "7ac7b154ec44046a23735ef87a8ff426",
+    "url": "/static/media/chevron-up.7ac7b154.svg"
+  },
+  {
+    "revision": "9c7f363c4453d99285f28a74db278478",
+    "url": "/static/media/balance-scale.9c7f363c.svg"
+  },
+  {
+    "revision": "8f85aa86679ae8d8590808749dbcaf48",
+    "url": "/static/media/file.8f85aa86.svg"
+  },
+  {
+    "revision": "df6259b7cd30fc64c1b495eee7b27917",
+    "url": "/static/media/Woman.df6259b7.svg"
+  },
+  {
+    "revision": "72ece140da29c82ff6c7248e38a4feb1",
+    "url": "/static/media/Man.72ece140.svg"
+  },
+  {
+    "revision": "f8511eb8e53c38cd54da2878e89b7f66",
+    "url": "/static/media/arrow-left.f8511eb8.svg"
+  },
+  {
+    "revision": "57df66f4afe8430a98f32ff93d48420b",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.dd9a76dc732b9d51a5a1e1f09d34fb9a.js"
+  "/precache-manifest.3bdcf5e68713b82c99c1f37a23b58dab.js"
 );
 
 workbox.clientsClaim();
