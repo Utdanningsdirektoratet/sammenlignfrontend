25bb46dd5a4a8b7490fcbe50b6d5935603aebef8
Ivar Nesje <ivarne@gmail.com>
2018-11-28 20:09:55 +0100

    Add plotly as dependency and add a simple example

--- src/components/comparisonsConfig/Yrke.tsx
+++ src/components/comparisonsConfig/Yrke.tsx
@@ -14,8 +14,9 @@ import LonnWrapper from "../visualizations/Lonn/LonnWrapper";
 import ArbeidsledighetWrapper from "../visualizations/Arbeidsledighet/ArbeidsledighetWrapper";
 import NoData from "../visualizations/Old/NoData";
 import PercentageBar from "../visualizations/Generic/PercentageBar";
-import Translate from "../app/Translate";
+import Translate, { TranslateString } from "../app/Translate";
 import EntreprenorskapWrapper from "../visualizations/Entreprenorskap/EntreprenorskapWrapper";
+import Plotly from "../visualizations/Generic/Plotly";
 
 const Yrke: SammenligningTemplate[] = [
   {
@@ -68,68 +69,30 @@ const Yrke: SammenligningTemplate[] = [
     title: "Sektorer",
     widget_id: "sektor",
     path: "/rest/main",
-    render: (data: MainElement) => {
+    render: (element: MainElement) => {
       return (
-        <table>
-          <tbody>
-            {typeof data.sektor_antall_personer !== "undefined" ? (
-              <tr>
-                <th>
-                  <Translate nb="Antall personer" />
-                </th>
-                <td>{data.sektor_antall_personer}</td>
-              </tr>
-            ) : null}
-            {typeof data.sektor_antall_arbeidsledig !== "undefined" ? (
-              <tr>
-                <th>
-                  <Translate nb="Antall arbeidsledige" />
-                </th>
-                <td>{data.sektor_antall_arbeidsledig}</td>
-              </tr>
-            ) : null}
-            {typeof data.sektor_antall_ikkearbeid !== "undefined" ? (
-              <tr>
-                <th>
-                  <Translate nb="Antall ikke i arbeid" />
-                </th>
-                <td>{data.sektor_antall_ikkearbeid}</td>
-              </tr>
-            ) : null}
-            {typeof data.sektor_antall_iutdanning !== "undefined" ? (
-              <tr>
-                <th>
-                  <Translate nb="Antall i utdanning" />
-                </th>
-                <td>{data.sektor_antall_iutdanning}</td>
-              </tr>
-            ) : null}
-            {typeof data.sektor_antall_offentlig !== "undefined" ? (
-              <tr>
-                <th>
-                  <Translate nb="Antall offentlig sektor" />
-                </th>
-                <td>{data.sektor_antall_offentlig}</td>
-              </tr>
-            ) : null}
-            {typeof data.sektor_antall_personer !== "undefined" ? (
-              <tr>
-                <th>
-                  <Translate nb="Antall privat sektor" />
-                </th>
-                <td>{data.sektor_antall_personer}</td>
-              </tr>
-            ) : null}
-            {typeof data.sektor_antall_selvstendig !== "undefined" ? (
-              <tr>
-                <th>
-                  <Translate nb="Antall selvstendig næringsdrivende" />
-                </th>
-                <td>{data.sektor_antall_selvstendig}</td>
-              </tr>
-            ) : null}
-          </tbody>
-        </table>
+        <Plotly
+          data={[
+            {
+              values: [
+                element.sektor_antall_privat || 0,
+                element.sektor_antall_offentlig || 0,
+              ],
+              labels: [TranslateString("privat"), TranslateString("offentlig")],
+              type: "pie",
+            },
+          ]}
+          layout={{
+            autosize: true,
+            title: "Sektor",
+            margin: {
+              l: 10,
+              r: 10,
+              b: 10,
+            },
+          }}
+          config={{ responsive: true }}
+        />
       );
     },
   },
