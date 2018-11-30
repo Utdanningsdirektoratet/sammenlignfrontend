fd74ca6c7bc9424e06fd8249b43efc7e70f1884d
Kristin Annabel Folland <kristinannabel.folland@gmail.com>
2018-11-30 15:29:56 +0100

    Now only showing correct number of empty cells set by config.

--- src/components/pages/ComparisonPage/ComparisonRow.tsx
+++ src/components/pages/ComparisonPage/ComparisonRow.tsx
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 
 import styles from "./ComparisonRow.module.scss";
 import Translate from "../../app/Translate";
+import {
+  NUM_COMPARES_MOBILE,
+  NUM_COMPARES_DESKTOP,
+} from "../../../data/config";
 
 type Props = {
   children: JSX.Element[];
@@ -13,22 +17,25 @@ function ComparisonRow({ children }: Props) {
   const emptyCellsText = (
     <Translate nb="Du kan søke for å legge til flere kolonner" />
   );
+
+  const emptyCellsLength =
+    window.innerWidth < 768
+      ? NUM_COMPARES_MOBILE - rowLength
+      : NUM_COMPARES_DESKTOP - rowLength;
+
+  let emptyCells = [];
+  for (var i = 0; i < emptyCellsLength; i++) {
+    emptyCells.push(
+      <div className={`${styles.empty_cells_placeholder}`} key={i}>
+        <div>{emptyCellsText}</div>
+      </div>
+    );
+  }
+
   return (
     <div className={`${styles.row_style}`}>
       {children}
-
-      <div className={`${styles.empty_cells_placeholder}`}>
-        <div>{emptyCellsText}</div>
-      </div>
-      <div className={`${styles.empty_cells_placeholder}`}>
-        <div>{emptyCellsText}</div>
-      </div>
-      <div className={`${styles.empty_cells_placeholder}`}>
-        <div>{emptyCellsText}</div>
-      </div>
-      <div className={`${styles.empty_cells_placeholder}`}>
-        <div>{emptyCellsText}</div>
-      </div>
+      {emptyCells}
     </div>
   );
 }
