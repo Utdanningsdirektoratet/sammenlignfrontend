7b090c27db321a1dd6324e5218c87f6d4838ca43
nicholas lium <nicholas.lium@udir.no>
2019-11-04 09:56:18 +0100

    UTDTEK-524, reduced height of visualization, removed some debugging code

--- src/components/pages/Frontpage.tsx
+++ src/components/pages/Frontpage.tsx
@@ -105,25 +105,6 @@ class Frontpage extends React.Component<Props & ScreenSizeProps> {
     let isDesk = innerWidth >= MIN_DESKTOP_PX ? `${styles.frontpage_undertitle}` : "";
     return (
       <PageChrome>
-        <Visualization
-          unoId="u_bioingeniorfag"
-          limit={8}
-          disaggregatedBy={["antall_offentlig", "antall_privat", "antall_ukjent_sektor"]}
-          disaggregateLabels={["Offentlig", "Privat", "Ukjent sektor"]}
-        />
-        <Visualization
-          unoId="u_bioingeniorfag"
-          limit={8}
-          disaggregatedBy={["antall_40", "over_40"]}
-          disaggregateLabels={["under 40", "Over 40"]}
-        />
-        <Visualization
-          unoId="u_bioingeniorfag"
-          limit={8}
-          disaggregatedBy={["antall_13", "antall_710", "other_experience"]}
-          disaggregateLabels={["1-3 years", "7-10 years", "Other"]}
-          layout={"tree"}
-        />
         <h1 className={`${styles.frontpage_header}`}>
           <Translate nb="Jeg vil sammenligne" />{" "}
           {innholdstype ? (
